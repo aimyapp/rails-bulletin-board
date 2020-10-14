2b999bd86c9bef12073993dfcdb3e9d1c304131d
aimy <aimy.app@gmail.com>
2020-10-14 23:30:26 +0900

    掲示板データの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,22 @@
 class BoardsController < ApplicationController
     def index
     end
-
+  
     def new
-        @board = Board.new
+      @board = Board.new
+        # デバッグ開始 dokcer attach webコンテナ(docker-compose psのweb)
+        # デバッグ終了 exit → Ctrl + C + Q
+        # binding.pry 
     end
-end
+  
+    def create
+      Board.create(board_params)
+      binding.pry
+    end
+  
+    private
+  
+    def board_params
+      params.require(:board).permit(:name, :title, :body)
+    end
+  end
