72b174e6b485c5edc1b7be031f0b21f78a4ccad8
aimy <aimy.app@gmail.com>
2020-10-17 20:42:43 +0900

    リソースベールルーティング、編集画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'boards#index'
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
-  get 'boards/:id', to: 'boards#show'
+  # get '/', to: 'boards#index'
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
+  # get 'boards/:id', to: 'boards#show'
+  # http://192.168.99.100:3000/rails/info/routes
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end
